eec24db367f675cdd6793dd52af973c782e7efd5
LeiMel <leizanemelo@gmail.com>
2025-09-01 13:07:12 -0300

    Atulizado o arquivo classifica_triangulo

--- classifica_triangulo.py
+++ classifica_triangulo.py
@@ -1,1 +1,18 @@
-# Sua solução aqui
+#Ler as entradas
+a, b, c = map(int, input('Digite o valor de A, B e C (separado por espaço): ' ).split())
+
+#Verificar se forma um triânguloc
+if a < b + c and b < a + c and c < a + b:
+#Verifica qual tipo de triângulo
+    if a == b and b == c:
+        print('equilátero')
+  elif a == b or b == c or a == c:
+        print('isósceles')
+    else:
+        print ('escaleno')
+#Se não formar um triângulo, mostrar msg informando
+else:
+    print('Não forma triângulo')
+
+
+
